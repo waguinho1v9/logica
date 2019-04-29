0d8c8f6feb51e3a0424f83fe3979d466261bfa32
waguinho1v9 <43183874+waguinho1v9@users.noreply.github.com>
2019-04-29 16:12:20 -0300

    Add files via upload

---
+++ switch/site.js
@@ -0,0 +1,59 @@
+/**
+ * @author Wagner Bento
+ */
+//var onde = window.location.pathname;
+//alert (onde)
+function abrirSite() {
+    let Entrada
+    Entrada = parseInt(frmAbrir.txtEntrada.value)
+    switch (Entrada) {
+        case 1:
+            window.location = "C:/Users/wagner.bsanto1/Desktop/logica%20wagner/logica/Calculodamedia/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 2:
+            window.location = "C:/Users/wagner.bsanto1/Desktop/logica%20wagner/logica/Churrascoladora/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 3:
+            window.location = "C:/Users/wagner.bsanto1/Desktop/logica%20wagner/logica/Circulo/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 4:
+            window.location = "C:/Users/wagner.bsanto1/Desktop/logica%20wagner/logica/Conversão Dolar e Real/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 5:
+            window.location = "C:/Users/wagner.bsanto1/Desktop/logica%20wagner/logica/Conversão Fahrenheit para Celsius/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 6:
+            window.location = "C:/Users/wagner.bsanto1/Desktop/logica%20wagner/logica/Tabuada/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 7:
+            window.location = "C:/Users/wagner.bsanto1/Desktop/logica%20wagner/logica/calculoIMC/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 8:
+            window.location = "C:/Users/wagner.bsanto1/Desktop/logica%20wagner/logica/jogoDado/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 9:
+            window.location = "C:/Users/wagner.bsanto1/Desktop/logica%20wagner/logica/jokenpo/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 10:
+            window.location = "C:/Users/wagner.bsanto1/Desktop/logica%20wagner/logica/precoDaHora/index.html";
+            break
+    }
+}
